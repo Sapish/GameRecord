f2634b0e179bbe6845d5c57ef0ea8a9349df9080
Oskar <oskardb@uia.no>
2025-04-08 22:25:07 +0200

    step 2

--- app.mjs
+++ app.mjs
@@ -1,1 +1,19 @@
- 
+
+import game from "./models/game.mjs";
+
+const gameDetails = {
+    title: "Concordia",
+    designer: "Mac Gerdts",
+    artist: "Marina Fahrenbach",
+    publisher: "PD-Verlag",
+    year: 2013,
+    players: "2-5",
+    time: "90 mins",
+    difficulty: "Medium",
+    url: "https://boardgamegeek.com/boardgame/124361/concordia",
+    playCount: 44,
+    personalRating: 9,
+};
+
+const myGame = new game(gameDetails);
+console.log(myGame);
